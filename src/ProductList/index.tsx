ed05b4369ad489b706345b5ae5b2c03f081649e5
olfa maslah <olfa@onoffapp.com>
2021-05-17 00:16:59 +0200

    putting back right data.json

--- src/ProductList/index.tsx
+++ src/ProductList/index.tsx
@@ -18,7 +18,7 @@ const ProductList: React.FC<ProductsListProps> = ({ selectedValue }) => {
         dispatch(productsActions.isLoading());//TODO will be move into GalleryView?
     }, [dispatch, products])
     const displayProducts = useSelector((state: ProductsReduxState) => filterProductsBySearchString(state));
-    console.log(displayProducts)
+    console.log('displayProducts', displayProducts)
     return (
         <>
             <ul>
