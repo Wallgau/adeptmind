4466d8fae78ab8265d62fc495353fd07525ebba3
olfa maslah <olfa@onoffapp.com>
2021-05-20 00:57:33 +0200

    add testing to productsDisplay styled-components

--- src/ProductDetails/productDetails.test.tsx
+++ src/ProductDetails/productDetails.test.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import ProductDetails from './index';
 import { ListImgLoad, ListTitleLoad, ListDescriptionLoad, LoadWrapper, LoadingAnimation, StyledImage, ListContainer, Container } from './productDetails.styles'
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const mockDispatch = jest.fn();
+
+jest.mock("react-redux", () => ({
+    useSelector: jest.fn(),
+    useDispatch: () => mockDispatch,
+}));
 
 test('it works', () => {
     const tree = renderer.create(<ListDescriptionLoad />).toJSON()
@@ -41,3 +53,14 @@ test('it works', () => {
     const tree = renderer.create(<Container selectedDisplay={'list'} />).toJSON()
     expect(tree).toMatchSnapshot()
 })
+
+const render = (method = shallow) => method(<ProductDetails product={{
+    "image": "navy_jacket.jpg",
+    "title": "Adidas Men's Arsenal Soccer Windbreaker Jacket",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio dolor doloremque molestiae totam! Dicta dolore dolores et, laudantium magnam molestias qui quo quod repudiandae sequi? Adipisci deserunt doloribus quis quos."
+}} />)
+it("renders one <ListContainer /> components", () => {
+    const productDetailsComponent = render();
+    expect(productDetailsComponent.find(ListContainer).length).toEqual(1);
+});
+
