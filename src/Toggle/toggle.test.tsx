0ed16b22a78fdf691997f6a95af279401149df08
olfa maslah <olfa@onoffapp.com>
2021-05-20 00:15:56 +0200

    debug testing for Toggle

--- src/Toggle/toggle.test.tsx
+++ src/Toggle/toggle.test.tsx
@@ -1,36 +1,50 @@
-import { PRODUCTS_DISPLAY_PROPERTIES } from '../constants';
-import { ToggleProps } from './toggle.types';
-import { Provider } from 'react-redux';
-import store from '../core/store';
-import Toggle from './index';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { PRODUCTS_DISPLAY_PROPERTIES } from "../constants";
+import Toggle from "./index";
+import Enzyme, { shallow, mount } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { ToggleInput, ToggleLabel } from "./toggle.styles";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('check Toggle display', () => {
-    const mockFn = jest.fn();
+const mockDispatch = jest.fn();
+
+jest.mock("react-redux", () => ({
+    useSelector: jest.fn(),
+    useDispatch: () => mockDispatch,
+}));
+
+const toggleValues = [
+    PRODUCTS_DISPLAY_PROPERTIES.GRID,
+    PRODUCTS_DISPLAY_PROPERTIES.LIST,
+];
+
+const render = (method = shallow) => method(<Toggle values={toggleValues} />);
+describe("check Toggle display", () => {
     const event = {
         preventDefault() { },
-        target: { value: 'list' }
+        target: { value: "list" },
     };
-    const toggleValues = [PRODUCTS_DISPLAY_PROPERTIES.GRID, PRODUCTS_DISPLAY_PROPERTIES.LIST];
-    const toggleComponent = shallow<ToggleProps>(
-        <Provider store={store}>
-            <Toggle values={toggleValues} />
-        </Provider>
-    );
-    toggleComponent.debug();
 
-    it('togglesValues.length should match the amount of inputs', () => {
-        expect(toggleComponent.find('input').length).toEqual(toggleValues.length);
-    })
+    it("togglesValues.length should match the amount of inputs", () => {
+        const toggleComponent = render();
+        expect(toggleComponent.find(ToggleInput).length).toEqual(
+            toggleValues.length
+        );
+    });
 
-    it('togglesValues.length should match the amount of label', () => {
-        expect(toggleComponent.find('.toggleLabel').length).toEqual(toggleValues.length);
-    })
-    it('togglesValue to be called with list ', () => {
-        toggleComponent.find('radio-8db96e0b-2b40-4c44-9cd1-747494d5e50e').simulate('change', event);
-        expect(mockFn).toBeCalledWith('list');
-    })
+    it("togglesValues.length should match the amount of label", () => {
+        const toggleComponent = render();
+        expect(toggleComponent.find(ToggleLabel).length).toEqual(
+            toggleValues.length
+        );
+    });
+
+    it("togglesValue to be called with list", () => {
+        const toggleComponent = render(mount);
+        toggleComponent.find('input[value="list"]').simulate("change", event);
+        expect(mockDispatch).toBeCalledWith({
+            type: "setStorageView",
+            view: "list",
+        });
+    });
 });
